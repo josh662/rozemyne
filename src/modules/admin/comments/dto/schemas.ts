fdb85497103bb211d0f014a2c19412907625a520
Oliveira04 <augustocoimbra01@hotmail.com>
2025-09-25 17:32:17 -0300

    feat: comentários

---
+++ src/modules/admin/comments/dto/schemas.ts
@@ -0,0 +1,45 @@
+import { z } from 'zod';
+
+import {
+  createListSchema,
+  dateSchema,
+  generateZodSchema,
+} from 'src/modules/app/dto';
+import { origin } from './interfaces';
+
+export const schema = generateZodSchema(origin);
+
+export const createSchema = z.object({
+  mediaId: z.string().trim(),
+  userId: z.string().trim(),
+  mediaComponentId: z.string().trim().optional(),
+  content: z.string(),
+  spoiler: z.boolean().optional(),
+});
+
+export const listSchema = createListSchema({
+  id: z.string().trim().optional(),
+  mediaId: z.string().trim().optional(),
+  userId: z.string().trim().optional(),
+  mediaComponentId: z.string().trim().optional(),
+  content: z.string().trim().optional(),
+  spoiler: z.boolean().optional(),
+  createdAt: dateSchema.optional(),
+});
+
+export const findSchema = z.object({
+  id: z.string().trim(),
+  mediaId: z.string().trim(),
+});
+
+export const updateSchema = z.object({
+  id: z.string().trim(),
+  mediaId: z.string().trim(),
+  content: z.string().trim().optional(),
+  spoiler: z.boolean().optional(),
+});
+
+export const removeSchema = z.object({
+  id: z.string().trim(),
+  mediaId: z.string().trim(),
+});
