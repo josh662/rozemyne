112aa1948be725ce0b7137e3f17b7f35a049867b
Kobelnik <luisfeliperkobelnik@gmail.com>
2025-09-25 20:09:51 -0300

    feat: CRUD dos conteúdos salvos

--- src/modules/admin/index.ts
+++ src/modules/admin/index.ts
@@ -2,6 +2,7 @@ import { CommentsModule } from './comments/module.module';
 import { ListsModule } from './lists/module.module';
 import { MediasModule } from './medias/module.module';
 import { MediaComponentsModule } from './mediaComponents/module.module';
+import { SavesModule } from './saves/module.module';
 import { UsersModule } from './users/module.module';
 import { VerificationsModule } from './verifications/module.module';
 
@@ -9,6 +10,7 @@ export const adminModules = [
   CommentsModule,
   ListsModule,
   MediaComponentsModule,
+  SavesModule,
   MediasModule,
   UsersModule,
   VerificationsModule,
