f058236fee513c31e8efef20f21955b7a8d74aac
Oliveira04 <augustocoimbra01@hotmail.com>
2025-09-25 19:58:23 -0300

    feat: Listas de leituras

--- src/modules/admin/index.ts
+++ src/modules/admin/index.ts
@@ -1,9 +1,13 @@
+import { CommentsModule } from './comments/module.module';
+import { ListsModule } from './lists/module.module';
 import { MediasModule } from './medias/module.module';
 import { MediaComponentsModule } from './mediaComponents/module.module';
 import { UsersModule } from './users/module.module';
 import { VerificationsModule } from './verifications/module.module';
 
 export const adminModules = [
+  CommentsModule,
+  ListsModule,
   MediaComponentsModule,
   MediasModule,
   UsersModule,
